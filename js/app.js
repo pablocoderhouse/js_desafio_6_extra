6fdbce32e314610001cdaaacebfe18daac34b7a3
Pablo Gonzalez <you@example.com>
2021-03-29 13:57:41 -0300

    desafio extra clase 6

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//Desafio Extra 6 precios ordenados de menor a mayor
+
+const cabañas = [{ id: 1,  cabaña: "economica", precio: 2000, capacidad: 2  },
+                  {  id: 2,  cabaña: "familiar", precio: 5000, capacidad: 4 },
+                  {  id: 3,  cabaña: "ejecutiva", precio: 8000, capacidad: 6 },
+                  {  id: 4,  cabaña: "hostel", precio: 1000, capacidad: 1 }];
+
+console.log(cabañas,)
+
+console.log([2000, 5000, 8000, 1000].sort((a, b) => a - b));
+
+
+
+
+
+
+
